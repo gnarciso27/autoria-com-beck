e7cbda95e48a761a0113d198360da7ba7bba7ea1
afonsosecundaria <dechinelo2023@gmail.com>
2025-06-13 15:13:46 -0300

    guigay

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,45 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-
+const mysql = require("mysql");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "autoriaweb",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/api/cadastro', (req, res) =>{
+
+    const { nome, sobrenome, email, telefone, senha } = req.body;
+
+    if (!nome || !sobrenome || !email || !telefone || !senha) {
+        return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+    }
+
+    const nomeCompleto = `${nome} ${sobrenome}`;
+    const tipo_usuario = "estudante";
+
+    const sql = "INSERT INTO usuarios (nome, email, telefone, senha_hash, tipo_usuario) VALUES (?, ?, ?, ?, ?)";
+    const values = [nomeCompleto, email, telefone, senha, tipo_usuario];
+
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            console.error("Erro ao inserir usuário:", err);
+            return res.status(500).json({ error: "Erro ao cadastrar." });
+        }
+        return res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
+    });
+})
+
+
 
 const cursosPath = path.join(__dirname, 'data', 'cursos.json');
 
