e6fbdfc65aed55b82f698ad67e939a1d55cf469b
Guilherme Narciso Trigueiro <20231144010023@ifrn.local>
2025-06-27 09:32:23 -0300

    perfil atualizado

--- perfil/perfil.js
+++ perfil/perfil.js
@@ -95,4 +95,12 @@ const user = {
   });
 
   
+  const toggleBtn = document.getElementById('toggleBtn');
+  const sidebar = document.getElementById('sidebar');
+
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('expanded');
+  });
+
+
   
